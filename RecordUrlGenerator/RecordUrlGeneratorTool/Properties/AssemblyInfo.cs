1a18654e787ed2e5c7cbbddb6bf8f4ffc9898e75
samsim0n <samuel.simon.dev@gmail.com>
2026-03-16 09:09:26 +0100

    version and author name

--- RecordUrlGenerator/RecordUrlGeneratorTool/Properties/AssemblyInfo.cs
+++ RecordUrlGenerator/RecordUrlGeneratorTool/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RecordUrlGeneratorTool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SamuelSIMON")]
 [assembly: AssemblyProduct("RecordUrlGeneratorTool")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2026.3.14")]
-[assembly: AssemblyFileVersion("1.2026.3.14")]
+[assembly: AssemblyVersion("1.2026.3.16")]
+[assembly: AssemblyFileVersion("1.2026.3.16")]
